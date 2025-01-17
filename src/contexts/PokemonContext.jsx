c3eb310cbab4b8cdc81ec7cda799c6493306dc7a
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-17 17:10:03 -0300

    contexto com adição do dotenv e remoção dos dados sensiveis

---
+++ src/contexts/PokemonContext.jsx
@@ -0,0 +1,68 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react-refresh/only-export-components */
+require('dotenv').config()
+import { createContext, useEffect, useState } from 'react'
+
+export const PokemonContext = createContext()
+
+const PokemonProvider = ({ children }) => {
+  const [pokemons, setPokemons] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [page, setPage] = useState(1)
+  const [filter, setFilter] = useState('')
+  const [totalPokemons, setTotalPokemons] = useState(0)
+  const [pokemonsPerPage] = useState(20)
+
+  useEffect(() => {
+    const api_key = process.env.API_KEY
+    setLoading(true)
+
+    const filterQuery = filter ? `&q=name:${filter}` : ''
+
+    fetch(
+      `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=${pokemonsPerPage}${filterQuery}`,
+      {
+        headers: {
+          'X-Api-Key': api_key,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemons(data.data)
+        setTotalPokemons(data.totalCount)
+        setLoading(false)
+      })
+      .catch((error) => {
+        console.log(`Erro ao buscar os dados: ${error}`)
+        setLoading(false)
+      })
+  }, [page, filter, pokemonsPerPage])
+
+  const changePage = (value) => {
+    setPage(value)
+  }
+
+  const changeFilter = (newFilter) => {
+    setFilter(newFilter)
+    setPage(1)
+  }
+
+  return (
+    <PokemonContext.Provider
+      value={{
+        pokemons,
+        loading,
+        page,
+        totalPokemons,
+        changePage,
+        changeFilter,
+        pokemonsPerPage,
+      }}
+    >
+      {children}
+    </PokemonContext.Provider>
+  )
+}
+
+export default PokemonProvider
