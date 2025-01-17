c81a4ec8a1e2d19ebeac2829198910985e7a93e1
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-16 22:11:05 -0300

    componente de filtro criado

---
+++ src/components/MultipleSelectPlaceholder/index.jsx
@@ -0,0 +1,91 @@
+import * as React from 'react'
+import { useTheme } from '@mui/material/styles'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import styles from './styles.module.css'
+
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+}
+
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder',
+]
+
+function getStyles(name, personName, theme) {
+  return {
+    fontWeight: personName.includes(name)
+      ? theme.typography.fontWeightMedium
+      : theme.typography.fontWeightRegular,
+  }
+}
+
+export default function MultipleSelectPlaceholder() {
+  const theme = useTheme()
+  const [personName, setPersonName] = React.useState([])
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    setPersonName(typeof value === 'string' ? value.split(',') : value)
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.label}>
+        <img src="/filterIcon.png" alt="" />
+        <span>Filtrar por: </span>
+      </div>
+      <FormControl sx={{ width: 300 }}>
+        <Select
+          multiple
+          displayEmpty
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput />}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return <em>Selecione</em>
+            }
+
+            return selected.join(', ')
+          }}
+          MenuProps={MenuProps}
+          inputProps={{ 'aria-label': 'Without label' }}
+        >
+          <MenuItem disabled value="">
+            <em>Placeholder</em>
+          </MenuItem>
+          {names.map((name) => (
+            <MenuItem
+              key={name}
+              value={name}
+              style={getStyles(name, personName, theme)}
+            >
+              {name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  )
+}
