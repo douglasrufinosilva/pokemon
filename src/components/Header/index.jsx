757d8a6c92ae34471d3cf9867a511ac6811e5995
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-17 23:41:10 -0300

    scroll to top adicionado a logo do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,18 @@
 import styles from './styles.module.css'
 
 const Header = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <img className={styles.img} src="/logo.svg" />
+        <button onClick={scrollToTop}>
+          <img className={styles.img} src="/logo.svg" />
+        </button>
         <span className={styles.text}>Documentação</span>
       </div>
     </header>
