ed57052539670e0c940370cd2020f56c56e24760
Douglas Rufino <douglasrufinosilva@gmail.com>
2025-01-19 23:46:59 -0300

    ajuste no vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '10.0.1.134',
-    port: 3000,
-  },
   plugins: [react()],
 })
